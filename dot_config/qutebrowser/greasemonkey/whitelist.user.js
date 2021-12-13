e79e1da47e7332a8f3fc4a1297e6b126c554202a
Guillaume Lostis <guillaume.lostis@preligens.com>
2021-12-13 09:18:53 +0100

    Misc changes

--- dot_config/qutebrowser/greasemonkey/whitelist.user.js
+++ dot_config/qutebrowser/greasemonkey/whitelist.user.js
@@ -1,6 +1,13 @@
 // ==UserScript==
 // @match https://docs.google.com/*
+// @match https://duckduckgo.com/*
+// @match https://*github.com/*
+// @match https://stackoverflow.com/*
 // ==/UserScript==
+
+// This greasemonkey script "whitelists" websites with the `match` statements
+// from qutebrowser's darkmode.
+// Source: https://github.com/qutebrowser/qutebrowser/issues/5542#issuecomment-782040210
 
 const meta = document.createElement('meta');
 meta.name = "color-scheme";
