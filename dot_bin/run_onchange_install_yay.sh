f840a1caa91a272681c83e7ab3bdcfcdc0173d87
Guillaume Lostis <guillaume.lostis@preligens.com>
2022-01-15 19:53:38 +0100

    Add script to install yay

---
+++ dot_bin/run_onchange_install_yay.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+if command -v makepkg &> /dev/null; then
+    if ! command -v yay &> /dev/null; then
+        git clone https://aur.archlinux.org/yay.git /tmp/yayrepo
+        cd /tmp/yayrepo
+        makepkg -si
+        rm -r /tmp/yayrepo
+    fi
+fi
