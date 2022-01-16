f1a426f3af8b64f858d3e3c0c94bd0137689c1eb
Guillaume Lostis <guillaume.lostis@preligens.com>
2022-01-16 14:06:28 +0100

    Minor edits

--- dot_bin/run_onchange_install_yay.sh
+++ dot_bin/run_onchange_install_yay.sh
@@ -2,7 +2,7 @@
 
 if command -v makepkg &> /dev/null; then
     if ! command -v yay &> /dev/null; then
-        git clone https://aur.archlinux.org/yay.git /tmp/yayrepo
+        git clone https://aur.archlinux.org/yay-bin.git /tmp/yayrepo
         cd /tmp/yayrepo
         makepkg -si
         rm -r /tmp/yayrepo
