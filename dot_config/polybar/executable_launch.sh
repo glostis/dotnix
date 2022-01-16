3d9fb7ecc283db4120416ede5bcbc416f8e66f70
Guillaume Lostis <guillaume.lostis@preligens.com>
2022-01-16 17:32:09 +0100

    Update polybar config

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -3,6 +3,8 @@
 # Wait until already running bar instances have been shut down
 while pgrep -x polybar >/dev/null; do killall -q polybar; sleep 0.1; done
 
+net_interface=$(ip link | grep '^[0-9]' | sed 's/^.* \([a-z][a-z0-9]*\): .*$/\1/' | grep '^wl')
+
 for m in $(xrandr | sed -n '/ connected /p' | sed 's/ /@/g'); do
     # Looks something like this (for 2 monitors)
     # @0:@+*DP-2@1920/598x1080/336+0+0@@DP-2
@@ -13,7 +15,7 @@ for m in $(xrandr | sed -n '/ connected /p' | sed 's/ /@/g'); do
         bars="${bars} center"
     fi
     for bar in $bars; do
-        MONITOR="$monitor" polybar -l info "$bar" > /tmp/polybar_"$monitor"_"$bar".log 2>&1 &
+        DEFAULT_NETWORK_INTERFACE="$net_interface" MONITOR="$monitor" polybar -l info "$bar" > /tmp/polybar_"$monitor"_"$bar".log 2>&1 &
     done
     sleep 1
 done
