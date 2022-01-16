c20248bfe7c4506bcd2e00f9d4c8551584eb8afa
Guillaume Lostis <guillaume.lostis@preligens.com>
2022-01-16 15:49:10 +0100

    Change post-install scripts

--- dot_bin/run_onchange_post_install.sh
+++ dot_bin/run_onchange_post_install.sh
@@ -2,5 +2,10 @@
 
 chsh -s /bin/zsh
 
+${HOME}/.bin/add_touchpad_conf
+${HOME}/.bin/edit_pacman_conf
+${HOME}/.bin/install_yay
+${HOME}/.bin/install_archlinux
+
 # Use `id $USER` to check to which groups the user currently belongs
 sudo usermod -G docker,video $USER
