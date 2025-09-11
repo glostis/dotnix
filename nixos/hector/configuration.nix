df6470f9bf547eb1d2895e8640de50b8d7fc7637
Guillaume Lostis <guillaume@lostis.com>
2025-09-11 15:59:09 +0200

    add hector config

---
+++ nixos/hector/configuration.nix
@@ -0,0 +1,54 @@
+{pkgs, ...}: {
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixos";
+    fsType = "ext4";
+  };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/boot";
+    fsType = "ext4";
+  };
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.initrd.availableKernelModules = ["ahci" "xhci_pci" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" "ext4"];
+
+  time.timeZone = "Europe/Paris";
+  i18n.defaultLocale = "en_US.UTF-8";
+  console.keyMap = "us";
+
+  users.users = {
+    root.hashedPassword = "!"; # Disable root login
+    glostis = {
+      isNormalUser = true;
+      extraGroups = ["wheel"];
+      openssh.authorizedKeys.keys = [
+        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCj00t57iGyCg3aR1o5/+X8qLrAEX/jMKtYJ9gyrJSU4jgdBSD+mM1RaoZlwOcc9EqbRk7MKMGsPTq8KvOP7/rZGlEzD5lJweShLHcgnI1ojofgUTXc3RzJwpIUDHPnOAMOBIy9X+1yIz0AXSU+UIWf2OLHINiLZS73p639iHl+lcmedhWR6bE1AVKfIe3FuHyvhGj0HOrwqZYXNdw0RH5c5Kbzhc4QTuOQuutypmXOatejDW1aO94Wz+uyRkkybHXYElJ+iN3vCsnvdxzOYVSiZB9ElTPCIBml6wUmojPfVCYhgYAAX3zEVmq8BjlUVnzcpLn9ceG4ICRd2O9riwjfslG9GzOFkHUnGDqT7nlJQUuP5fZ8T3MUyuHeYWLBxCmkSxc76M87ERSyXfmZsnZwYLGd4rUsYrjYpuyrdST5T9GrEoRNU2o0moKHSd4jRZ/w3ycTxcLAcS1GV+Dd1xXP8vx67apf+VwJdSYFp7/uXJhlGhFGm9hi25r3zS3JH1s= glostis@eugene"
+      ];
+    };
+  };
+
+  security.sudo.wheelNeedsPassword = false;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+  ];
+
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "no";
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [22];
+
+  system.stateVersion = "25.05";
+}
--- nixos/suzanne/configuration.nix
+++ nixos/suzanne/configuration.nix
@@ -1,39 +1,17 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}: {
+{pkgs, ...}: {
   imports = [
     # Include the results of the hardware scan.
-    # For VM, comment out for real laptop
-    # ./hardware-configuration-vm.nix
     ./hardware-configuration.nix
   ];
 
-  # Bootloader.
-  # For real laptop, commented out
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # For VM, comment out for real laptop
-  # boot.loader.grub.enable = true;
-  # boot.loader.grub.device = "/dev/sda";
-  # boot.loader.grub.useOSProber = true;
-
   networking.hostName = "suzanne";
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "Europe/Paris";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
@@ -148,16 +126,11 @@
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #  wget
+    vim
+    git
   ];
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
   services.locate = {
@@ -173,11 +146,5 @@
 
   programs.i3lock.enable = true;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
+  system.stateVersion = "24.05";
 }
