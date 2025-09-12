e5109f9a831c3328fd57e38779dc7bef5712743b
Guillaume Lostis <guillaume@lostis.com>
2025-09-12 10:26:30 +0200

    tweaks

--- nixos/hector/configuration.nix
+++ nixos/hector/configuration.nix
@@ -20,7 +20,8 @@
     root.hashedPassword = "!"; # Disable root login
     glostis = {
       isNormalUser = true;
-      extraGroups = ["wheel"];
+      extraGroups = ["wheel" "docker"];
+      shell = pkgs.zsh;
       openssh.authorizedKeys.keys = [
         "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCj00t57iGyCg3aR1o5/+X8qLrAEX/jMKtYJ9gyrJSU4jgdBSD+mM1RaoZlwOcc9EqbRk7MKMGsPTq8KvOP7/rZGlEzD5lJweShLHcgnI1ojofgUTXc3RzJwpIUDHPnOAMOBIy9X+1yIz0AXSU+UIWf2OLHINiLZS73p639iHl+lcmedhWR6bE1AVKfIe3FuHyvhGj0HOrwqZYXNdw0RH5c5Kbzhc4QTuOQuutypmXOatejDW1aO94Wz+uyRkkybHXYElJ+iN3vCsnvdxzOYVSiZB9ElTPCIBml6wUmojPfVCYhgYAAX3zEVmq8BjlUVnzcpLn9ceG4ICRd2O9riwjfslG9GzOFkHUnGDqT7nlJQUuP5fZ8T3MUyuHeYWLBxCmkSxc76M87ERSyXfmZsnZwYLGd4rUsYrjYpuyrdST5T9GrEoRNU2o0moKHSd4jRZ/w3ycTxcLAcS1GV+Dd1xXP8vx67apf+VwJdSYFp7/uXJhlGhFGm9hi25r3zS3JH1s= glostis@eugene"
       ];
@@ -48,8 +49,15 @@
     };
   };
 
+  virtualisation.docker.enable = true;
+
+  programs.zsh.enable = true;
+
   networking.hostName = "hector";
-  networking.firewall.allowedTCPPorts = [22];
+  networking.firewall = {
+    allowedTCPPorts = [22];
+    allowedUDPPorts = [12345];
+  };
 
   system.stateVersion = "25.05";
 }
